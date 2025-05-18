5687d5f9c5506466ce3e4910c0514913a36dbcd1
윤종환 <yunjonghwan99@gmail.com>
2025-05-18 22:00:07 +0900

    헤더 컴포넌트에서 검색 입력 및 버튼을 감싸는 div 추가

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -3,7 +3,10 @@ import Input from "../components/Input";
 export const Header = () => {
   return (
     <header className="w-full p-4 bg-white shadow-md flex justify-between items-center">
-      <Input type="text" placeholder="검색어를 입력하세요" className="border border-gray-300 rounded-md px-4 py-2 w-64"/>
+      <div className="w-full flex justify-center">
+        <Input type="text" placeholder="검색어를 입력하세요" className="border border-gray-300 rounded-md px-4 py-2 w-56"/>
+        <button type="button" className="bg-[#5ea2f7] text-white px-4 py-2 rounded-lg hover:bg-[#7bb7ff]">검색</button>
+      </div>
     </header>
   );
 };

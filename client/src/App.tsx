bfe819f7a76c2d6b33c15423597e2e4fc332ecf1
윤종환 <yunjonghwan99@gmail.com>
2025-05-18 21:17:45 +0900

    react-router-dom 및 Tailwind CSS 통합, 불필요한 CSS 파일 삭제

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,4 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // pages 폴더의 예시 컴포넌트 import (실제 파일명에 맞게 수정)
 import Home from './pages/Home'

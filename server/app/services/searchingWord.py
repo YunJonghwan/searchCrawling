7eb4a1e4f3e36aeb0c07b9f582d833c709cada73
윤종환 <yunjonghwan99@gmail.com>
2025-05-24 21:30:39 +0900

    검색을 지정할 때 화면이 나오기 전에 검색을 진행한 것으로 판단하여 일단 대기를 하고 진행하도록 코드 변경

--- server/app/services/searchingWord.py
+++ server/app/services/searchingWord.py
@@ -1,21 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import time
 
 def searchingWord(word):
   driver = webdriver.Chrome()  # 크롬 드라이버 실행
-  driver.get("https://news.naver.com/")
-
-  search = driver.find_element(By.CLASS_NAME, "Nicon_search")
-  search.click()
-
-  search_box = driver.find_element(By.NAME, "query")
-  search_box.send_keys(word)
-  search_box.send_keys(Keys.RETURN)
   
-  titles = driver.find_elements(By.CSS_SELECTOR, "span.sds-comps-text")
-  print("기사 타이틀", titles)
+  search_url = f"https://search.naver.com/search.naver?where=news&ie=utf8&sm=nws_hty&query={word}"
+  
+  driver.get(search_url)
+  
+  WebDriverWait(driver, 10).until(
+    EC.presence_of_all_elements_located((By.CLASS_NAME, "sds-comps-text-type-headline1"))
+  )
+  titles = driver.find_elements(By.CLASS_NAME, "sds-comps-text-type-headline1")
+  title_list = [title.text for title in titles]
+  print("기사 타이틀", title_list)
 
   time.sleep(5)
   driver.quit()

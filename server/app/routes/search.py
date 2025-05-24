811cfa1d856cacc94ebf0e4baec8ae260e0ba8f2
윤종환 <yunjonghwan99@gmail.com>
2025-05-24 20:23:31 +0900

    search_items 함수에서 검색어를 처리하기 위해 searchingWord 함수 호출 추가

--- server/app/routes/search.py
+++ server/app/routes/search.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, request, jsonify
+from services.searchingWord import searchingWord
 
 # Blueprint 객체 생성 (이름, 모듈명)
 search_bp = Blueprint('search', __name__)
@@ -7,4 +8,5 @@ search_bp = Blueprint('search', __name__)
 @search_bp.route('/search')
 def search_items():
     word = request.args.get('word')
+    searchingWord(word);
     return jsonify({"results": word})

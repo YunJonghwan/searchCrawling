855d8a20a6707bdb95143b5c0d63a646bc92dac4
윤종환 <yunjonghwan99@gmail.com>
2025-05-24 20:22:31 +0900

    searchingWord 함수 추가: 주어진 단어로 네이버 뉴스 검색 수행

---
+++ server/app/services/searchingWord.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+def searchingWord(word):
+  driver = webdriver.Chrome()  # 크롬 드라이버 실행
+  driver.get("https://news.naver.com/")
+
+  search = driver.find_element(By.CLASS_NAME, "Nicon_search")
+  search.click()
+
+  search_box = driver.find_element(By.NAME, "query")
+  search_box.send_keys(word)
+  search_box.send_keys(Keys.RETURN)
+
+  time.sleep(5)
+  driver.quit()

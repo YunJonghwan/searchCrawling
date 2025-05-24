f4203e7b9c922418f00a7e02fb2d391d6048014e
윤종환 <yunjonghwan99@gmail.com>
2025-05-24 20:20:33 +0900

    search_items 함수에서 검색어 처리 로직 간소화 및 콘솔 출력 제거

--- server/app/routes/search.py
+++ server/app/routes/search.py
@@ -6,10 +6,5 @@ search_bp = Blueprint('search', __name__)
 # Blueprint에 라우트 등록
 @search_bp.route('/search')
 def search_items():
-    search = request.args.get('word')
-    print("search 값:", search)
-    if search == "고양이":
-        results = "성공"
-    else:
-        results = []
-    return jsonify({"results": results})
+    word = request.args.get('word')
+    return jsonify({"results": word})

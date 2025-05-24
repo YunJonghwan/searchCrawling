89b4229f43eaa6c62e69fb8c6e3096b09b03290b
윤종환 <yunjonghwan99@gmail.com>
2025-05-24 19:59:09 +0900

    CORS 설정 추가: localhost:3030에서의 요청 허용

--- server/app/app.py
+++ server/app/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.search import search_bp
 
 app = Flask(__name__)
+CORS(app, origins="http://localhost:3030");
 
 # Blueprint 등록
 app.register_blueprint(search_bp)

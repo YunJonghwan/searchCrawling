139a1cdbf57b1b8a0f694ba7618e853f242a5798
윤종환 <yunjonghwan99@gmail.com>
2025-05-24 20:19:49 +0900

    dotenv 로드 및 CORS 설정에서 CLIENT_PORT 환경 변수 사용

--- server/app/app.py
+++ server/app/app.py
@@ -1,12 +1,16 @@
 from flask import Flask
 from flask_cors import CORS
+from dotenv import load_dotenv
+import os
 from routes.search import search_bp
 
+load_dotenv()
+
 app = Flask(__name__)
-CORS(app, origins="http://localhost:3030");
+CORS(app, origins=f"http://localhost:{os.getenv('CLIENT_PORT')}");
 
 # Blueprint 등록
 app.register_blueprint(search_bp)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=os.getenv('DEBUG'), port=os.getenv('SERVER_PORT'));

e91dd816f0e244db4776832ea3c546f1a548c38b
윤종환 <yunjonghwan99@gmail.com>
2025-06-01 16:10:33 +0900

    NewsDTO의 subtitle 필드를 summary로 변경하고, searchingWord 함수에서 기사 제목과 요약을 올바르게 처리하도록 수정

--- server/app/schemas/news.py
+++ server/app/schemas/news.py
@@ -3,6 +3,6 @@ from dataclasses import dataclass
 @dataclass
 class NewsDTO:
     title: str
-    subtitle: str
+    summary: str
     image: str  # 이미지 URL, 없으면 빈 문자열 등으로 처리
     url: str
--- server/app/services/searchingWord.py
+++ server/app/services/searchingWord.py
@@ -17,37 +17,34 @@ def searchingWord(word):
       EC.presence_of_all_elements_located((By.CLASS_NAME, "sds-comps-text-type-headline1"))
     )
     titles = driver.find_elements(By.CLASS_NAME, "sds-comps-text-type-headline1")
-    subTitles = driver.find_elements(By.CLASS_NAME, "sds-comps-text-type-body1")
-    img_items = driver.find_elements(By.CLASS_NAME, "sds-rego-thumb-overlay")
-    urls = driver.find_elements(By.CLASS_NAME, "_wQLg_3VK6OUyNYiF7VY")
+    summarys = driver.find_elements(By.CLASS_NAME, "sds-comps-text-type-body1")
+    imgs = driver.find_elements(By.CLASS_NAME, "sds-rego-thumb-overlay")
 
     # 각 div 안의 a 태그의 href 추출
-    img_links = []
-    url_links = []
-    for img_div, url_div in zip(img_items, urls):
+    img_list = []
+    url_list = []
+    title_list = []
+    summary_list = []
+    for img_div, title, summary in zip(imgs, titles, summarys):
+        title_list.append(title.text)
+        summary_list.append(summary.text)
         try:
             img_tag = img_div.find_element(By.TAG_NAME, "img")
-            a_tag = url_div.find_element(By.TAG_NAME, "a");
-            
-            src = img_tag.get_attribute("src")
-            href = a_tag.get_attribute('href');
-        except Exception:
-            src = ""
-            href = ""
-        img_links.append(src)
-        url_links.append(href)
-
-    title_list = [title.text for title in titles]
-    subTitle_list = [sub.text for sub in subTitles]
+            img_list.append(img_tag.get_attribute("src"))
+            a_tag = title.find_element(By.XPATH, "./ancestor::a[1]")
+            url_list.append(a_tag.get_attribute("href"))
+        except Exception as e:
+            print("스크랩 에러:", e)
 
     # DTO 객체로 묶어서 반환
     news_list = []
-    for title, subtitle, img_link, url in zip(title_list, subTitle_list, img_links, url_links):
-        news_list.append(NewsDTO(title=title, subtitle=subtitle, image=img_link, url=url))
+    for title, sub, img_link, url in zip(title_list, summary_list, img_list, url_list):
+        news_list.append(NewsDTO(title=title, summary=sub, image=img_link, url=url))
         time.sleep(0.5)
     return news_list
   except Exception as e:
     print("크롤링 에러:", e)
     return []
   finally:
+    print("드라이버 종료")
     driver.quit()

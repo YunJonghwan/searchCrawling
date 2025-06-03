fe42e0eca870db32fb9c4d27293d27e8f308d6db
윤종환 <yunjonghwan99@gmail.com>
2025-06-03 23:34:30 +0900

    korean_article_analysis 함수에서 불용어 필터링 추가 및 반환 형식을 OrderedDict로 변경

--- server/app/services/korean_article_analysis.py
+++ server/app/services/korean_article_analysis.py
@@ -1,15 +1,18 @@
 from konlpy.tag import Okt
 from collections import Counter
+from collections import OrderedDict
 
 def korean_article_analysis(content):
     okt = Okt()
-    # content가 리스트라면 모든 텍스트를 하나의 문자열로 합침
+    stopwords = {"그리고", "하지만", "이", "그", "저", "등", "의", "에", "은", "는", "이", "가", "을", "를", "도", "로", "와", "과", "에서", "에게", "한", "하다"}
+    
     if isinstance(content, list):
         text = ' '.join(content)
     else:
         text = str(content)
-    nouns = okt.nouns(text)
+
+    nouns = [noun for noun in okt.nouns(text) if noun not in stopwords]
     word_count = Counter(nouns)
     # 빈도수 기준 내림차순 정렬된 (단어, 카운트) 리스트 반환
     sorted_word_count = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
-    return sorted_word_count
+    return OrderedDict(sorted_word_count)

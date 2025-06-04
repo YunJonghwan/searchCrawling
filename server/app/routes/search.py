e897bd5b104e8230b4557e94b4fcf63047ecce80
윤종환 <yunjonghwan99@gmail.com>
2025-06-04 21:04:57 +0900

    article_content 함수에서 기사 가져오기 시작 메시지 추가

--- server/app/routes/search.py
+++ server/app/routes/search.py
@@ -22,6 +22,7 @@ def news_items():
 @search_bp.route('/article')
 def article_content():
     news = crawlingNews()
+    print("Fetching articles...")
     content = []
     for article in news:
         text = scrapArticle(article.url)

db8b29030857dc42633fc2b89dfacf9dd6043c7a
윤종환 <yunjonghwan99@gmail.com>
2025-06-06 21:26:39 +0900

    last_news 변수를 추가하여 기사 내용을 가져올 때 최신 뉴스를 재사용하도록 수정

--- server/app/routes/search.py
+++ server/app/routes/search.py
@@ -7,6 +7,8 @@ from services.korean_article_analysis import korean_article_analysis
 # Blueprint 객체 생성 (이름, 모듈명)
 search_bp = Blueprint('search', __name__)
 
+last_news = None
+
 # Blueprint에 라우트 등록
 @search_bp.route('/search')
 def search_items():
@@ -17,11 +19,14 @@ def search_items():
 @search_bp.route('/news')
 def news_items():
     news = crawlingNews()
+    global last_news
+    last_news = news
     return jsonify({"results": news})
 
 @search_bp.route('/article')
 def article_content():
-    news = crawlingNews()
+    global last_news
+    news = last_news if last_news is not None else crawlingNews()
     content = []
     for article in news:
         text = scrapArticle(article.url)

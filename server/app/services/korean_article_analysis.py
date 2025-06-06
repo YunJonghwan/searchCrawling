05aeeb13b4407510a3b641a715f900bc8c538933
윤종환 <yunjonghwan99@gmail.com>
2025-06-06 22:25:11 +0900

    불용어 목록에 추가 단어 포함 및 한글 명사 추출 조건 개선

--- server/app/services/korean_article_analysis.py
+++ server/app/services/korean_article_analysis.py
@@ -1,13 +1,14 @@
 from konlpy.tag import Okt
 from collections import Counter
 from collections import OrderedDict
+import re
 
 def korean_article_analysis(content):
     okt = Okt()
     stopwords = {
         "그리고", "하지만", "이", "그", "저", "등", "의", "에", "은", "는", "이", "가", "을", "를", "도", "로", "와", "과", "에서", "에게",
         "한", "하다", "수", "것", "고", "며", "말", "전", "더", "밎", "시", "위", "데", "시", "데", "온", "약", "안", "점", "개", "명", "나",
-        "건", "뒤", "레", "번", "섭", "주", "중", "청", "게", "관"
+        "건", "뒤", "레", "번", "섭", "주", "중", "청", "게", "관", "홍", "확", "환", "함", "티", "통", "탈", "통", "티", "채", "초", "및"
     }
     
     if isinstance(content, list):
@@ -15,7 +16,8 @@ def korean_article_analysis(content):
     else:
         text = str(content)
 
-    nouns = [noun for noun in okt.nouns(text) if noun not in stopwords]
+    # 한글로만 이루어진 명사만 추출
+    nouns = [noun for noun in okt.nouns(text) if noun not in stopwords and re.match(r'^[가-힣]+$', noun)]
     word_count = Counter(nouns)
     # 빈도수 기준 내림차순 정렬된 (단어, 카운트) 리스트 반환
     sorted_word_count = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
